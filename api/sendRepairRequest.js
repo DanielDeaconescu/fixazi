92e27087e6e1b16029331e124c319ea951a25853
Daniel Deaconescu <daniel.deaconescu23@gmail.com>
2025-05-11 20:36:17 +0300

    further changes

--- api/sendRepairRequest.js
+++ api/sendRepairRequest.js
@@ -16,6 +16,9 @@ export default async function handler(req, res) {
   const form = formidable({ multiples: false });
 
   form.parse(req, async (err, fields, files) => {
+    console.log("Fields:", fields);
+    console.log("Files:", files);
+
     if (err) {
       console.error("Form parsing error:", err);
       return res.status(500).json({ message: "Form parsing failed" });
--- js/script.js
+++ js/script.js
@@ -198,3 +198,13 @@ setupHoverEffects(mediaQuery.matches);
 mediaQuery.addEventListener("change", (e) => {
   setupHoverEffects(e.matches);
 });
+
+// custom file upload functionality
+document.getElementById("file-upload").addEventListener("change", function () {
+  const fileNameSpan = document.getElementById("file-name");
+  if (this.files.length > 0) {
+    fileNameSpan.textContent = this.files[0].name;
+  } else {
+    fileNameSpan.textContent = "Niciun fișier selectat";
+  }
+});
